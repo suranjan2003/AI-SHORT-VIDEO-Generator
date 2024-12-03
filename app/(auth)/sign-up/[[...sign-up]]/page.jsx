cbe22f77ff3230c34836499f98f55815bde95c22
suranjan2003 <karmakarsuranjan12@gmail.com>
2024-12-03 11:48:44 +0530

    sign up page fixed

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,17 @@
 import { SignUp } from '@clerk/nextjs'
+import Image from 'next/image'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div>
+            <Image src={'/login-image.jpg'}
+            alt='sign-up' width={500} height={500}
+            className='w-full object-contain'/>
+        </div>
+        <div className='flex justify-center items-center'>
+            <SignUp/>
+        </div>
+    </div>
+  )
 }
