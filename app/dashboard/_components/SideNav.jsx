074468297a29a46309d6dcf4dbee8ecf0a2bf5ff
suranjan2003 <karmakarsuranjan12@gmail.com>
2024-12-05 23:37:22 +0530

    side nav introduced

---
+++ app/dashboard/_components/SideNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <div className='w-64 h-screen shadow-md p-5'>SideNav</div>
+  )
+}
+
+export default SideNav
--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './_components/Header'
+import SideNav from './_components/SideNav';
 
 const DashboardLayout = ({children}) => {
   return (
     <div>
+        <div className='hidden md:block h-screen bg-white fixed mt-[64px] w-64'>
+            <SideNav/>
+        </div>
         <div>
             <Header/>
-            {children}
+            <div className='md:ml-64'>
+                {children}
+            </div>
         </div>
     </div>
   )
